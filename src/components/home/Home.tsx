5b9771e1efc17931d679803c5590a2fa8b04acf7
RicardoCS18 <ricardog.carreras@gmail.com>
2023-12-19 21:51:14 -0500

    Home

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <>
+      <div>Arena</div>
+      <div>Duel Disk</div>
+      <div>Card Shop</div>
+      <div>Forum</div>
+    </>
+  )
+}
+
+export default Home
